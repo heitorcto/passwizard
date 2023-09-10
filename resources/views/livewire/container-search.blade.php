6a5c5a336885bcbdacc99acb9928c16eb6f7d055
heitorcto <heitorcasella@gmail.com>
2023-09-10 12:57:12 -0300

    refactor: trocar toggle de busca por favoritos por checkbox

--- resources/views/livewire/container-search.blade.php
+++ resources/views/livewire/container-search.blade.php
@@ -3,10 +3,10 @@
         <div class="card-body">
             <h2 class="card-title justify-center mb-3">Buscar</h2>
             <x-input label="Container" type="text" wire="container" holder="Wizard..." keydown="setContainers" />
-            <div class="form-control w-52">
-                <label class="cursor-pointer label">
+            <div class="form-control w-28">
+                  <label class="label cursor-pointer">
                     <span class="label-text">Favoritos</span>
-                    <input type="checkbox" class="toggle toggle-primary" wire:click="setFavorites" />
+                    <input type="checkbox" class="checkbox checkbox-primary" wire:click="setFavorites" />
                 </label>
             </div>
         </div>

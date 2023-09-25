89b733e2a2c2c9cfdf4a217ea7c159fd211252c1
heitorcto <heitorcasella@gmail.com>
2023-09-25 20:30:04 -0300

    feat: criação das configurações de perfil

--- resources/views/livewire/user-profile-settings.blade.php
+++ resources/views/livewire/user-profile-settings.blade.php
@@ -8,16 +8,24 @@
             <h2 class="card-title">Perfil</h2>
             <form wire:submit="saveChanges">
                 <div class="grid grid-cols-2 gap-3">
-                    <x-input holder="John Wizzard" label="Nome Completo" wire="name" />
+                    <x-input label="Nome completo" placeholder="John Wizzard" wire:model="name" />
 
-                    <x-input holder="john@wizzard.com" label="E-mail" type="email" wire="email" />
-
-                    {{-- <div wire:dirty wire:ignore>Unsaved changes...</div> --}}
+                    <x-input label="E-mail" placeholder="john@wizzard.com" type="email" wire:model="email" />
                 </div>
-                <div class="card-actions justify-end mt-3">
-                    <button class="btn btn-primary">Salvar</button>
+
+                <x-input label="Digite sua senha para confirmar as alterações" placeholder="****" type="password" wire:model="password" />
+
+                <div class="card-actions justify-end mt-3 flex items-center">
+                    <button class="btn btn-primary" wire:target="name, email">Salvar</button>
                 </div>
             </form>
+        </div>
+    </div>
+
+    <div class="toast toast-center" wire:dirty wire:target="name, email">
+        <div class="alert alert-warning">
+            <i class="fa-solid fa-triangle-exclamation"></i>
+            <span>Existem alterações não salvas.</span>
         </div>
     </div>
 </div>
